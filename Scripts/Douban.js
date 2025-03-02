5decf337f7011e35dcffd4e984d5b4a47147dba1
PoetryU <68501042+PoetryU@users.noreply.github.com>
2025-03-02 14:40:38 +0800

    modified.

--- Scripts/Douban.js
+++ Scripts/Douban.js
@@ -1,122 +1,102 @@
 /*
-Douban Movie Add-ons for Surge by Neurogram
+    Douban Movie Add-ons for Quantumult X by Neurogram
 
-modified Poet
+        - 豆瓣电影网页插件
+        - 快捷跳转自定义网站搜索
+        - 展示在映流媒体平台（TMDB API）
 
- - 豆瓣电影移动版网页增强
- - 快捷跳转 茶杯狐、奈菲影视、555电影 搜索
- - 快捷收藏电影至 Airtable
+    使用说明
 
-使用说明
+        [rewrite_local]
+        https:\/\/m(ovie)*\.douban\.com\/(movie\/)*subject\/.+ url script-response-body Douban.js
 
-[Script]
+        [MITM]
+        hostname = m.douban.com, movie.douban.com
 
-Surge4.0:
-茶杯狐、奈菲影视、555电影
-http-response ^https://m.douban.com/movie/subject/.+ script-path=https://sci.poetyin.com/Public/Scripts/Douban.js,requires-body=true,max-size=307200
-
-Airtable 收藏
-http-request ^https://m.douban.com/movie/subject/.+\?seen=\d script-path=https://sci.poetyin.com/Public/Scripts/Douban.js
-
-QX1.0.0:
-茶杯狐、奈菲影视、555电影
-^https://m.douban.com/movie/subject/.+ url script-response-body https://sci.poetyin.com/Public/Scripts/Douban.js
-
-Airtable 收藏
-^https://m.douban.com/movie/subject/.+\?seen=\d url script-request-body https://sci.poetyin.com/Public/Scripts/Douban.js
-[MITM]
-hostname = m.douban.com
-
-收藏功能，需自行修改代码，点击 想看 / 看过 触发收藏
+    Author:
+        Telegram: Neurogram
+        GitHub: Neurogram-R
 */
 
-let url = $request.url
-let movieId = url.match(/subject\/(\d+)/)
-let seen = url.match(/\?seen=(\d)$/)
-let collect = false  //收藏功能，默认关闭，需自行配置
-if (!seen) {
-    let body = $response.body
-    let title = body.match(/"sub-title">([^<]+)/)
-    if (title) {
 
-        let mweb = `<a href="https://www.cupfox.com/search?key=${title[1]}"><img src="https://files.catbox.moe/c8vszl.png" height="25" width="34.78" style="vertical-align: text-top;" /></a>
-        <a href="https://www.nfmovies.com/search.php?searchword=${title[1]}"><img src="https://files.catbox.moe/gog93l.png" height="25" width="20.11" style="vertical-align: text-top;" /></a>
-<a href="https://www.o8tv.com/index.php/vod/search.html?wd=${title[1]}&submit="><img src="https://files.catbox.moe/27bzxu.png" height="25" width="25" style="vertical-align: text-top;" /></a>`
+const url = $request.url
+const movieId = url.match(/subject\/(\d+)/)?.[1]
+const platform = url.includes('movie.douban.com') ? 'web' : 'mobile'
 
-        body = body.replace(/("sub-title">.+?)(<\/div>)/, `$1${mweb}$2`)
+const tmdb_region = 'US' // TMDB 查询区域
+const tmdb_api_key = '' // TMDB API Key
 
-        if (collect) {
-            body = body.replace(/<a.+pbtn.+wish.+>/, `<a href="${url}?seen=0">`)
-            body = body.replace(/<a.+pbtn.+collect.+>/, `<a href="${url}?seen=1">`)
-        }
+// 可自定义添加网站搜索（格式：['名称', '搜索链接', '图标链接']，%@ 代表电影标题）
+const watch_web_data = [
+    ['247看', 'https://247kan.com/search?q=%@', 'https://247kan.com/favicon.ico'],
+    ['Cupfox', 'https://www.cupfox.in/search?q=%@', 'https://picx.zhimg.com/80/v2-de36e385e59fcca2df694b76f108431a.png'],
+    ['LIBIVO', 'https://www.libvio.fun/search/-------------.html?wd=%@', 'https://www.libvio.fun/statics/img/favicon.ico']
+]
 
-        $done({ body })
-    } else {
-        $done({})
-    }
-} else {
-    let options = {
-        url: `https://frodo.douban.com/api/v2/movie/${movieId[1]}?apiKey=0ac44ae016490db2204ce0a042db2916`,
-        headers: {
-            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.3(0x18000323) NetType/WIFI Language/en",
-            "Referer": "https://servicewechat.com/wx2f9b06c1de1ccfca/82/page-frame.html"
-        }
-    }
-    $httpClient.get(options, function (error, response, data) {
-        let info = JSON.parse(data)
-        if (error) {
-            $notification.post('获取影片信息失败', error, "");
-        } else if (data.msg == "movie_not_found") {
-            $notification.post('豆瓣电影', data.msg, "");
-        } else {
-            let casts = ""
-            for (var i = 0; i < info.casts.length; i++) {
-                casts = casts + info.casts[i].name + " / "
-            }
-            let directors = ""
-            for (var k = 0; k < info.directors.length; k++) {
-                directors = directors + info.directors[k].name + " / "
-            }
-            let title = info.title + "  " + info.original_title
-            let table = {
-                url: "https://api.airtable.com/v0/BASE_ID/Douban",
-                headers: {
-                    Authorization: "API_KEY"
-                },
-                body: {
-                    records: [
-                        {
-                            "fields": {
-                                "Title": title,
-                                "Description": info.summary,
-                                "Poster": [
-                                    {
-                                        "url": info.images.large
-                                    }
-                                ],
-                                "Seen": seen[1] == 1 ? true : false,
-                                "Actors": casts.replace(/\s\/\s$/, ""),
-                                "Director": directors.replace(/\s\/\s$/, ""),
-                                "Genre": info.genres.toString(),
-                                "Douban": "https://movie.douban.com/subject/" + movieId[1],
-                                "Rating": info.rating.average,
-                                "Year": info.year
-                            }
-                        }
-                    ]
-                }
-            }
-            $httpClient.post(table, function (error, response, data) {
-                data = JSON.parse(data)
-                if (error) {
-                    $notification.post('收藏失败', error, "");
-                } else if (data.records) {
-                    $notification.post('豆瓣电影', title + " 收藏成功", "");
-                } else {
-                    $notification.post('收藏失败', data.error.type, data.error.message);
-                }
-            })
-        }
+function send_request(options) {
+    return new Promise((resolve, reject) => {
+        $task.fetch(options).then(response => {
+            resolve(JSON.parse(response.body))
+        })
     })
-    $done({ url: url.replace(/\?seen=\d/, "") })
 }
+
+async function douban_addons() {
+
+    let body = $response.body
+    const title = body.match(/"sub-title">([^<]+)/)?.[1] ?? body.match(/<i class="">(.+)?的剧情简介<\/i>/)?.[1]
+
+    if (!title) $done({})
+
+    if (tmdb_api_key) {
+
+        const douban_result = await send_request({
+            url: `https://frodo.douban.com/api/v2/movie/${movieId}?apiKey=0ac44ae016490db2204ce0a042db2916`,
+            method: "GET",
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.3(0x18000323) NetType/WIFI Language/en',
+                'Referer': 'https://servicewechat.com/wx2f9b06c1de1ccfca/82/page-frame.html'
+            }
+        })
+
+        if (['movie', 'tv'].includes(douban_result.type) && douban_result.original_title) {
+
+            const tmdb_query = await send_request({
+                url: `https://api.themoviedb.org/3/search/${douban_result.type}?api_key=${tmdb_api_key}&query=${encodeURIComponent(douban_result.original_title.replace(/Season \d+$/, ''))}&page=1`,
+                method: "GET"
+            })
+
+            if (tmdb_query.results[0]) {
+
+                const tmdb_providers = await send_request({
+                    url: `https://api.themoviedb.org/3/${douban_result.type}/${tmdb_query.results[0].id}/watch/providers?api_key=${tmdb_api_key}`,
+                    method: "GET"
+                })
+
+                if (tmdb_providers.results[tmdb_region]?.flatrate) {
+
+                    for (const provider of tmdb_providers.results[tmdb_region].flatrate) {
+                        watch_web_data.push([provider.provider_name, '', `https://image.tmdb.org/t/p/original${provider.logo_path}`])
+                    }
+
+                }
+            }
+
+        }
+
+    }
+
+    const html_data = []
+
+    for (let i = 0; i < watch_web_data.length; i++) {
+        html_data.push(`<a href="${watch_web_data[i][1].replace(/%@/, title)}"><img src="${watch_web_data[i][2]}" height="25" style="width: auto; vertical-align: text-top;" /></a>`)
+    }
+
+    if (platform == 'web') body = body.replace(/(<span property="v:itemreviewed">(.|\n)+?)<\/h1>/, `$1${html_data.join('\n')}</h1>$2`)
+    if (platform == 'mobile') body = body.replace(/("sub-title">.+?)(<\/div>)/, `$1<br>${html_data.join('\n')}$2`)
+
+    $done({ body })
+
+}
+
+douban_addons()
